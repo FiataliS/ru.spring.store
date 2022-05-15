73bf23aea4402242266fdf15353a695517c42096
FiataliS <FiataliS@gmail.com>
2022-05-15 21:31:22 +0500

    minimal corrections

--- src/main/java/ru/spring/store/Repositories/ProductRepository.java
+++ src/main/java/ru/spring/store/Repositories/ProductRepository.java
@@ -2,19 +2,14 @@ package ru.spring.store.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import ru.spring.store.Model.Product;
-import ru.spring.store.Model.Users;
 
-import javax.persistence.NamedNativeQuery;
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
-    List<Product> findAllByPriceBetween(Integer min,Integer max);
+    List<Product> findAllByPriceBetween(Integer min, Integer max);
 
     @Query(value = "SELECT u.id, u.name, u.price from Product as u INNER JOIN purchases p ON (p.product_id = u.id) where p.user_id = :id", nativeQuery = true)
-    List getAllProductToUsers(Long id);
+    List<Product> getAllProductToUsers(Long id);
 }
--- src/main/java/ru/spring/store/Repositories/UsersRepository.java
+++ src/main/java/ru/spring/store/Repositories/UsersRepository.java
@@ -2,17 +2,16 @@ package ru.spring.store.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import ru.spring.store.Model.Product;
 import ru.spring.store.Model.Users;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface UsersRepository extends JpaRepository <Users, Long> {
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 
     @Query(value = "SELECT u.id, u.name from Users as u INNER JOIN purchases as p ON (p.user_id = u.id) where p.product_id = :id", nativeQuery = true)
-    List getAllUsersToProduct(Long id);
+    List<Users> getAllUsersToProduct(Long id);
 
 
 }
